107094d4639126bcbf87045ddf46395e9df3923d
Smoochy1 <arjunagastya110#@gmail.com>
2024-03-25 14:13:02 +0530

    uploading modified setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1',
     packages=['Polygon'],
     description='This is a package containing reactange ,square , triangle and circle classes and it has sub packages that have functions like area and perimeter which can be used on those object',
-    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Smoochy15',
     author_email='your.email@example.com',
